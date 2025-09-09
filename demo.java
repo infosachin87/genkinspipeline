202e28ad96136da07824a88cb9f19a9ef88cc15f
infosachin87 <infosachin87@gmail.com>
2025-09-08 23:13:20 -0700

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,7 +1,22 @@
-public class HelloWorld 
-{
-    public static void main(String[] args)
-  {
-        System.out.println("Hello, World!");
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        out.println("<html><body>");
+        out.println("<h1>Hello, this is a simple Java Servlet web app!</h1>");
+        out.println("</body></html>");
     }
 }
