521ca5e3626db6e7c50c4b65a72b79cf85168710
infosachin87 <infosachin87@gmail.com>
2025-09-08 23:17:11 -0700

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,22 +1,20 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
-    
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        
-        out.println("<html><body>");
-        out.println("<h1>Hello, this is a simple Java Servlet web app!</h1>");
-        out.println("</body></html>");
+@SpringBootApplication
+public class DemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApp.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, this is a Spring Boot web application!";
     }
 }
